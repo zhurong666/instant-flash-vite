349994a63d4bccee4a155808437f407ed1c9c56f
zhuyun <2798670451@qq.com>
2024-10-29 14:13:01 +0800

    基本实现

--- vite.config.ts
+++ vite.config.ts
@@ -12,34 +12,32 @@ export default defineConfig({
         }
     },
     server: {
-        proxy: {
-            // 字符串简写写法
-            // '/foo': 'http://localhost:4567',
-            // 选项写法
+        proxy: process.env.VITE_PROXY === 'true' ? {
             '/api': {
                 target: 'http://139.9.87.80:8080',
                 changeOrigin: true,
                 rewrite: (path) => path.replace(/^\/api/, '')
-            },
-            // 正则表达式写法
-            // '^/fallback/.*': {
-            //     target: 'http://jsonplaceholder.typicode.com',
-            //     changeOrigin: true,
-            //     rewrite: (path) => path.replace(/^\/fallback/, '')
-            // },
-            // 使用 proxy 实例
-            // '/api': {
-            //     target: 'http://jsonplaceholder.typicode.com',
-            //     changeOrigin: true,
-            //     configure: (proxy, options) => {
-            //         proxy 是 'http-proxy' 的实例
-                // }
-            // },
-            // Proxying websockets or socket.io
-            // '/socket.io': {
-            //     target: 'ws://localhost:3000',
-            //     ws: true
-            // }
-        }
+            }
+        } : undefined
+        // 正则表达式写法
+        // '^/fallback/.*': {
+        //     target: 'http://jsonplaceholder.typicode.com',
+        //     changeOrigin: true,
+        //     rewrite: (path) => path.replace(/^\/fallback/, '')
+        // },
+        // 使用 proxy 实例
+        // '/api': {
+        //     target: 'http://jsonplaceholder.typicode.com',
+        //     changeOrigin: true,
+        //     configure: (proxy, options) => {
+        //         proxy 是 'http-proxy' 的实例
+        // }
+        // },
+        // Proxying websockets or socket.io
+        // '/socket.io': {
+        //     target: 'ws://localhost:3000',
+        //     ws: true
+        // }
     }
+}
 })
