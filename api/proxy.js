ccffbe2cff9a20601b47fad81e139d0a76512321
zhuyun <2798670451@qq.com>
2024-10-29 16:34:30 +0800

    基本实现6

--- api/proxy.js
+++ api/proxy.js
@@ -1,13 +1,13 @@
 // api/proxy.js
-import {createProxyMiddleware} from 'http-proxy-middleware';
+import { createProxyMiddleware } from 'http-proxy-middleware';
 
-export default (req, res) => {
-    let target = ''
+const proxyHandler = (req, res) => {
+    let target = '';
     // 根据请求路径，设置代理目标地址
     if (req.url.startsWith('/backend')) {
-        target = 'http://139.9.87.80:8080' // 替换成实际的 API 地址
+        target = 'http://139.9.87.80:8080'; // 替换成实际的 API 地址
     }
-    console.log("我被重写了", target)
+    console.log("我被重写了", target);
     // 使用代理中间件转发请求
     createProxyMiddleware({
         target,
@@ -15,5 +15,7 @@ export default (req, res) => {
         pathRewrite: {
             '^/backend/': '/', // 重写路径，把 `/backend` 移除
         },
-    })(req, res)
-}
+    })(req, res);
+};
+
+export default proxyHandler;
