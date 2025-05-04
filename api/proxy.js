82878fc49dd34fc06d5c92e3c45aaea7ebb23e96
zhuyun <2798670451@qq.com>
2025-05-04 16:09:13 +0800

    vercel

--- api/proxy.js
+++ api/proxy.js
@@ -5,7 +5,8 @@ const proxyHandler = (req, res) => {
     let target = '';
     // 根据请求路径，设置代理目标地址
     if (req.url.startsWith('/backend')) {
-        target = 'http://139.9.87.80:8080'; // 替换成实际的 API 地址
+        target = 'https://api.yunzou.cloudns.org:10088/'; // 替换成实际的 API 地址
+        // target = 'https://minio.yunzou.cloudns.org:19000/'; // 替换成实际的 API 地址
     }
     console.log("我被重写了", target);
     // 使用代理中间件转发请求
--- src/router/index.ts
+++ src/router/index.ts
@@ -123,7 +123,7 @@ const routes: RouteRecordRaw[] = [
 ]
 
 const router = createRouter({
-    history: createWebHistory(import.meta.env.BASE_URL),
+    history: createWebHashHistory(import.meta.env.BASE_URL),
     routes,
 })
 
